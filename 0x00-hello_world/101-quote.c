8956a4dc19556ae8507f4b26bd77a71c4e9d04a5
FridahMuthoniMurithi <fridahmuriithi24@gmail.com>
2023-02-17 11:22:07 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 #include <unistd.h>
+
 /**
  * main - A c program that prints a line to the standard error
  * Return: 1 (Success)
  */
-
 int main(void)
 {
 	write(2,
-	"and that piece of art is useful\" -Dora Korpar, 2015-10-19\n", 59 char long [stderr]:[Anything](0 char long));
+	"and that piece of art is useful\" -Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
