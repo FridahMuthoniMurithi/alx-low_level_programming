cdb26e7a489376dbb55e3cc09a1bd45d38cad616
FridahMuthoniMurithi <fridahmuriithi24@gmail.com>
2023-02-18 00:24:00 -0800

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -24,7 +24,7 @@ int main(void)
 	{
 		printf("Last digit of %d id %d and is 0\n", n, a);
 	}
-	else if (a < 6 && a! = 0)
+	else if (a < 6 && a!= 0)
 	{
 		printf("Last diigit of %d is %d and is less than 6 and not 0\n", n, a);
 	}
